ed26fb0fc1ae59037fda090881328a0af6ddd9a3
koouu <kotatata420@gmail.com>
2022-02-17 01:38:36 +0900

    2/16

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/labstack/echo/v4"
 )
+func getHello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello !World!")
+}
 
 func main() {
 	e := echo.New()  // echo を利用する
     // GET リクエストでパスが `/` のとき第２引数の関数を実行する
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/",getHello )
 
     // 1323 ポートでリッスンを開始。 start がエラーを起こしたら Fatal を起こしてログに記録する
 	e.Logger.Fatal(e.Start(":1323"))
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module fail_app_server
 
 go 1.17
 
-require github.com/labstack/echo/v4 v4.6.3 // indirect
+require github.com/labstack/echo/v4 v4.6.3
